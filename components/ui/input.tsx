ae8a8a65633ee778feb951a9da212468dc9746fc
adamzhang <adamzhang@futunn.com>
2024-09-21 18:08:40 +0800

    feat: add prod build

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -2,10 +2,11 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+// 删除这个空接口
+// interface InputProps
+//   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
+const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(
   ({ className, type, ...props }, ref) => {
     return (
       <input
